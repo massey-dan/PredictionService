82c96f0a7942b68597a7bb77a1906f35694e5dde
massey-dan <example10101@googlemail.com>
2025-04-24 16:43:48 +0100

    Add Spring Boot project
    From https://start.spring.io/

---
+++ src/test/java/tech/splash/PredictionService/PredictionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.splash.PredictionService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredictionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
